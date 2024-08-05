8e47400a0c42cab75475a59b0bd740bf895435c2
Bentley Settin <bfs7357@uncw.edu>
2024-08-05 11:04:38 -0400

    updates

--- Firmware/AtlasConductivity_20240724/src/AtlasConductivity_20240724.cpp
+++ Firmware/AtlasConductivity_20240724/src/AtlasConductivity_20240724.cpp
@@ -8,6 +8,35 @@
 
 // Include Particle Device OS APIs
 #include "Particle.h"
+#include "SdFat.h"
+#include "SdFatTestSuite.h"
+
+ //------------------SD SPI Configuration Details--------------------------------
+const int SD_CHIP_SELECT = A5;
+SdFat sd;
+
+// Project specific
+#define address 100              //default I2C ID number for EZO pH Circuit.
+
+//------------------State variables
+// not yet used but placeholders in case of additional states
+enum State {
+  DATALOG_STATE,
+  PUBLISH_STATE,
+  SLEEP_STATE
+};
+State state = DATALOG_STATE;
+
+// Define whether (1) or not (0) to publish
+#define PUBLISHING 0
+
+unsigned long stateTime = 0;
+char data[120];
+
+//------------------Turn off cellular for prelim testing; turn on for deployment
+//SYSTEM_MODE(MANUAL); // uncomment for prelim testing
+SYSTEM_MODE(SEMI_AUTOMATIC); // uncomment for deployment
+SYSTEM_THREAD(ENABLED);
 
 // Global objects
 SerialLogHandler logHandler;
@@ -16,41 +45,67 @@ void setup(void);
 void loop(void);
 void string_pars();
 
-SYSTEM_MODE(SEMI_AUTOMATIC); // uncomment for deployment
-SYSTEM_THREAD(ENABLED);
+// Various timing constants
+const unsigned long MAX_TIME_TO_PUBLISH_MS = 20000; // Only stay awake for this time trying to connect to the cloud and publish
+// const unsigned long TIME_AFTER_PUBLISH_MS = 4000; // After publish, wait 4 seconds for data to go out
 
-// Project specific
-#define address 100              //default I2C ID number for EZO pH Circuit.
-
-char computerdata[20];           //we make a 20 byte character array to hold incoming data from a pc/mac/other.
-byte received_from_computer = 0; //we need to know how many characters have been received.
-bool serial_event = false;       //a flag to signal when data has been received from the pc/mac/other.
-byte code = 0;                   //used to hold the I2C response code.
-char ec_data[24];                //we make a 24 byte character array to hold incoming data from the pH circuit.
-byte in_char = 0;                //used as a 1 byte buffer to store in bound bytes from the EC Circuit.
-byte i = 0;                      //counter used for ec_data array.
-int delay_time = 1400;           //used to change the delay needed depending on the command sent to the EZO Class pH Circuit.
-
-char *ec;                        //char pointer used in string parsing.
+// ***** IMPORTANT!!!
+// If SECONDS_BETWEEN_MEASUREMENTS < 600, must use 
+// .network(NETWORK_INTERFACE_CELLULAR, SystemSleepNetworkFlag::INACTIVE_STANDBY);
+// in sleep configuration to avoid reconnection penalty
+const unsigned long SECONDS_BETWEEN_MEASUREMENTS = 3600; // What should sampling period be?
+// ***** IMPORTANT!!! See note above this const.
 
 
-float ec_float;                  //float var used to hold the float value of the pH.
+void setup(void) {
+  if (PUBLISHING==1) {
+    Particle.connect();
+  }
+  else{
+    Cellular.off(); // turn off cellular for prelim testing (uncomment)
+  }
 
+  // delay(5000); // to see response from begin command
 
-void setup()                     //hardware initialization.
-{
-  Serial.begin(9600);            //enable serial port.
-  Wire.begin();                  //enable I2C port.
+  Serial.begin(9600);
+  Log.info("Maxbotix Test");
+
 }
 
+void loop(void) {
+  // Enter state machine
+  switch (state) {
+    
+/////////////////////////DATALOG STATE////////////////////////
 
-void serialEvent() {           //this interrupt will trigger when the data coming from the serial monitor(pc/mac/other) is received.
-  received_from_computer = Serial.readBytesUntil(13, computerdata, 20); //we read the data sent from the serial monitor(pc/mac/other) until we see a <CR>. We also count how many characters have been received.
-  computerdata[received_from_computer] = 0; //stop the buffer from transmitting leftovers or garbage.
-  serial_event = true;           //set the serial event flag.
-}
+   case DATALOG_STATE: { 
+    char computerdata[20];           //we make a 20 byte character array to hold incoming data from a pc/mac/other.
+    byte received_from_computer = 0; //we need to know how many characters have been received.
+    bool serial_event = false;       //a flag to signal when data has been received from the pc/mac/other.
+    byte code = 0;                   //used to hold the I2C response code.
+    char ec_data[24];                //we make a 24 byte character array to hold incoming data from the pH circuit.
+    byte in_char = 0;                //used as a 1 byte buffer to store in bound bytes from the EC Circuit.
+    byte i = 0;                      //counter used for ec_data array.
+    int delay_time = 1400;           //used to change the delay needed depending on the command sent to the EZO Class pH Circuit.
 
-void loop() {                   //the main loop will take a reading every 5 seconds.
+    char *ec;                        //char pointer used in string parsing.
+
+    float ec_float;                  //float var used to hold the float value of the pH.
+
+    void setup()                     //hardware initialization.
+    {
+      Serial.begin(9600);            //enable serial port.
+      Wire.begin();                  //enable I2C port.
+    }
+
+
+    void serialEvent() {           //this interrupt will trigger when the data coming from the serial monitor(pc/mac/other) is received.
+      received_from_computer = Serial.readBytesUntil(13, computerdata, 20); //we read the data sent from the serial monitor(pc/mac/other) until we see a <CR>. We also count how many characters have been received.
+      computerdata[received_from_computer] = 0; //stop the buffer from transmitting leftovers or garbage.
+      serial_event = true;           //set the serial event flag.
+    } 
+
+    void loop() {                   //the main loop will take a reading every 5 seconds.
 
 //    computerdata[0] = 'r'; //Take a reading
     computerdata[0] = 'R';
@@ -102,12 +157,12 @@ void loop() {                   //the main loop will take a reading every 5 seco
     delay(5000-delay_time); //this will pause until 5 seconds have elapsed from previous reading
   }
 
-void string_pars() {                  //this function will break up the CSV string into its 4 individual parts. EC|TDS|SAL|SG.
+    void string_pars() {                  //this function will break up the CSV string into its 4 individual parts. EC|TDS|SAL|SG.
                                       //this is done using the C command “strtok”.
 
-  ec = strtok(ec_data, ",");          //let's pars the string at each comma.
+      ec = strtok(ec_data, ",");          //let's pars the string at each comma.
 
-  Log.info("ec: %s", ec);
+      Log.info("ec: %s", ec);
 
   //uncomment this section if you want to take the values and convert them into floating point number.
   /*
@@ -116,4 +171,114 @@ void string_pars() {                  //this function will break up the CSV stri
   sal_float=atof(sal);
   sg_float=atof(sg);
   */
+
+ // Start SD stuff
+    File myFile;
+
+    // Initialize the library
+    if (!sd.begin(SD_CHIP_SELECT, SPI_FULL_SPEED)) {
+      Log.info("failed to open card");
+    }
+
+    // open the file for write at end like the "Native SD library"
+    if (!myFile.open("distance.csv", O_RDWR | O_CREAT | O_AT_END)) {
+      Log.info("opening distance.csv for write failed");
+    }
+    else{ // if file does open, save to SD; otherwise, proceed to publish
+      // Save to SD card
+      myFile.print(data);
+      myFile.print("\n"); // put next data on new line
+      myFile.close();
+    }
+
+    if (PUBLISHING==1) {
+      state = PUBLISH_STATE;
+    }
+    else{
+      state = SLEEP_STATE;
+    }
+
+  }
+  break;
 }
+
+///////////////////////PUBLISH STATE////////////////////////
+
+  case PUBLISH_STATE: {
+
+    // Prep for cellular transmission
+    bool isMaxTime = false;
+    stateTime = millis();
+
+    while (!isMaxTime) {
+      //connect particle to the cloud
+      if (Particle.connected() == false) {
+        Particle.connect();
+        Log.info("Trying to connect");
+      }
+
+      // If connected, publish data buffer
+      if (Particle.connected()) {
+
+        Log.info("publishing data");
+
+        // bool (or Future) below requires acknowledgment to proceed
+        bool success = Particle.publish(eventName, data, 60, PRIVATE, WITH_ACK);
+        Log.info("publish result %d", success); 
+
+        isMaxTime = true;
+        state = SLEEP_STATE;
+      }
+      // If not connected after certain amount of time, go to sleep to save battery
+      else {
+        // Took too long to publish, just go to sleep
+        if (millis() - stateTime >= MAX_TIME_TO_PUBLISH_MS) {
+          isMaxTime = true;
+          state = SLEEP_STATE;
+          Log.info("max time for publishing reached without success; go to sleep");
+        }
+        Log.info("Not max time, try again to connect and publish");
+        delay(500);
+      }
+    }
+  }
+  break;
+
+  //////////////////////////////////////////////////////////////////////////////
+  /*** SLEEP_STATE ***/
+  /*** Get here from PUBLISH_STATE after attempted publish or DATALOG_STATE if PUBLISHING==0
+  ***/
+  case SLEEP_STATE: {
+    Log.info("going to sleep");
+    delay(500);
+
+    // Sleep time determination and configuration
+    int wakeInSeconds = secondsUntilNextEvent(); // Calculate how long to sleep 
+
+    config.mode(SystemSleepMode::ULTRA_LOW_POWER)
+      .gpio(D2, FALLING)
+      .duration(wakeInSeconds* 1000L); // Set seconds until wake
+      // .network(NETWORK_INTERFACE_CELLULAR, SystemSleepNetworkFlag::INACTIVE_STANDBY); // keeps the cellular modem powered, but does not wake the MCU for received data
+
+    // Ready to sleep
+    SystemSleepResult result = System.sleep(config); // Device sleeps here
+
+    // It'll only make it here if the sleep call doesn't work for some reason (UPDATE: only true for hibernate. ULP will wake here.)
+    Log.info("Feeling restless");
+    stateTime = millis();
+    state = DATALOG_STATE;
+  }
+  break;
+  }
+}
+
+int secondsUntilNextEvent() {
+
+  int current_seconds = Time.now();
+  int seconds_to_sleep = SECONDS_BETWEEN_MEASUREMENTS - (current_seconds % SECONDS_BETWEEN_MEASUREMENTS);
+
+  Log.info("Sleeping for %i", seconds_to_sleep);
+
+  return seconds_to_sleep;
+}
+
